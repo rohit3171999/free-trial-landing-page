9d20aa7d4dd8f9b9d9c101a15b1b568b24463420
rohit3171999 <rohit3171999@gmail.com>
2025-09-17 13:37:12 +0530

    add functional component in TrialOfferDeatils.jsx

---
+++ src/components/TrialOfferDeatils.jsx
@@ -0,0 +1,20 @@
+/*
+* Description:
+Details of the free trial (duration, inclusions, no credit card).
+
+Purpose:
+Answer FAQs upfront, build trust.
+* */
+export default function TrialOfferDetails(){
+    return (
+        <>
+            {/*<section> wrapper*/}
+
+            {/*    <h2> title → “What’s Included in Your Free Trial”*/}
+
+            {/*        <ul> bullet points for inclusions (premium features, no credit card, etc.)*/}
+
+            {/*            <p> highlight text (14 days free)*/}
+        </>
+    );
+}
