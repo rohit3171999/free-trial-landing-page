ea6e44791ad0decf6567c617d09b8fb4d533e3a4
rohit3171999 <rohit3171999@gmail.com>
2025-09-17 14:02:22 +0530

    add functional component in Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+/*
+* Description:
+Closing section with navigation and compliance links.
+
+Purpose:
+Provide important legal info and site navigation.
+* */
+export default function Footer(){
+    return (
+        <>
+            {/*<footer> wrapper*/}
+
+            {/*    <nav> with <ul> → links (About, Privacy Policy, Terms, Contact)*/}
+
+            {/*        <div> → social media icons (<a> + <svg> or <img>)*/}
+        </>
+
+    );
+}
