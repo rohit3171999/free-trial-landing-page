4c2621bfb0b0f21572ea9a12d8bab6d36bf8cea5
rohit3171999 <rohit3171999@gmail.com>
2025-09-17 13:20:27 +0530

    add functional component in ProductBenefits.jsx

---
+++ src/components/ProductBenefits.jsx
@@ -0,0 +1,24 @@
+/*
+* Description:
+Highlights 3–4 main benefits of using TaskFlow.
+
+Purpose:
+Communicate value in a scannable, visual format.
+* */
+export default function ProductBenefits(){
+    return (
+        <>
+            {/*<section> wrapper*/}
+
+            {/*    <h2> section title → “Why Teams Choose TaskFlow”*/}
+
+            {/*        <div> grid or <ul> → benefit cards*/}
+
+            {/*            <img> or <svg> icon*/}
+
+            {/*                <h3> benefit title*/}
+
+            {/*                    <p> short description*/}
+        </>
+    );
+}
