dc04a159cc68a698378a7db82fe90628f39b9795
rohit3171999 <rohit3171999@gmail.com>
2025-09-17 13:24:26 +0530

    add functional component in HowItWorks.jsx

---
+++ src/components/HowItWorks.jsx
@@ -0,0 +1,26 @@
+/*
+* Description:
+Simple steps explaining how the trial works.
+
+Purpose:
+Reduce friction by clarifying the process.
+* */
+export default function HowItWorks(){
+    return (
+        <>
+{/*            <section> wrapper*/}
+
+{/*                <h2> title → “How It Works”*/}
+
+{/*                    <ol> or flexbox step layout*/}
+
+{/*                        <li> step card*/}
+
+{/*                            <span> step number*/}
+
+{/*<h3> step title (e.g., “Sign Up”)*/}
+
+{/*<p> short description*/}
+        </>
+    );
+}
